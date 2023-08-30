f5c939073527ade8553ea01b55bd16b6880fc375
Madelu Lopes <142816035+Lulopis@users.noreply.github.com>
2023-08-30 10:12:48 -0300

    Adicionanda usuários

--- desafio.kt
+++ desafio.kt
@@ -1,13 +1,21 @@
 class usuário
-class contato ( val nome: String , var email: String)
+class contato ( var id: String , var email: String)
 
 fun main () {
 
 var usuário = usuário ()
 var contato = contato ("José" , "josé@gmail.com")
-println (contato.nome)
+println (contato.id)
 println (contato.email)
-}
+contato.id = "Marta"
+println (contato.id)
+contato.email = "marta@gmail.com"
+println (contato.email)
+contato.id = "Larissa"
+println (contato.id)
+contato.email = "larissa@gmail.com"
+println (contato.email)
 
+}
 
 
