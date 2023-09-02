66cbd6300392c99312deaac56169f1d26d2d6297
Madelu Lopes <142816035+Lulopis@users.noreply.github.com>
2023-09-02 13:22:26 -0300

    Criando Listas

---
+++ desafio5.kt
@@ -0,0 +1,15 @@
+// criando listas 
+
+import java.util.*
+
+fun main(){
+
+    var listaCompras =arrayOf("Arroz" , "Feijao" , "Batata" , "Uva" , "Banana" , "Carne" , "Leite" , "Ovos")
+    var indice = 0
+ 
+    
+for (indice in 0..7) {
+    println(listaCompras[indice])
+    
+}
+}
